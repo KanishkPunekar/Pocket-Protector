0297bbddea37cfc54f5c43ba58288fc331f34966
Kanishk Punekar <84027603+KanishkPunekar@users.noreply.github.com>
2023-09-20 10:02:49 +0530

    Add files via upload

---
+++ FinalExpenseTracker.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package finalexpensetracker;
+
+/**
+ *
+ * @author KANI
+ */
+public class FinalExpenseTracker {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new Login().setVisible(true);
+        // TODO code application logic here
+       
+    }
+    
+}
